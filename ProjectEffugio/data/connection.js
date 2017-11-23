58c63e9ea72f052f89cfd24fff4793eb1dc3a48a
jeeva91 <jee1199@gmail.com>
2017-11-23 16:48:12 -0500

    'completed_connections_travel'

--- ProjectEffugio/data/connection.js
+++ ProjectEffugio/data/connection.js
@@ -7,31 +7,78 @@ var ObjectID=require('mongodb').ObjectID;
 var bcrypt = Promise.promisifyAll(require("bcrypt"));
 
 let exportedMethods={
-    async getConnectionByID(_id){
+    async getConnectionById(_id){
         if(!_id){
-            throw 'you must provide an connection id to search'
+            throw 'you must provide an connection id to search';
         }
 
-        const connectionCollection = connectionList();
+        const connectionCollection = await connectionList();
+	    const listOfConnections = await connectionCollection.find({_id: _id}).limit(1).toArray();
+	    if (listOfConnections.length ===0) return null;
+	    return listOfConnections[0];
     },
-    async getConnectionByRequestorID(_userID){
-        const connectionCollection = connectionList();
+    async getConnectionByRequestorId(requestor_id){
+	    if(!requestor_id) throw 'provide an requestor id'
+        const connectionCollection = await connectionList();
+	    const listOfConnections = await connectionCollection.find({requestor_id: requestor_id}).toArray();
+	    if (listOfconnections.length ===0) return null;
+	    return listOfConnections;
+	
     },
-    async getConnectionbyConnectorID(_userID){
-        const connectionCollection = connectionList();
+    async getConnectionbyConnectedId(connected_id){
+	    if(!connector_id) throw 'provide an connector id'
+        const connectionCollection = await connectionList();
+	    const listOfConnections = await connectionCollection.find({connector_id: connector_id}).toArray();
+	    if (listOfconnections.length ===0) return null;
+	    return listOfConnections;
 
     },
     async getAllConnections(){
-        const connectionCollection = connectionList();
+        const connectionCollection = await connectionList();
+	    const listOfConnections = await connectionCollection.find().toArray();
+	    connections = [];
 
+
+	    for(var val of listOfConnections){
+	        oneConnection={};
+	        oneConnection._id = val._id;
+	        oneConnection.requestor_id = val.requestor_id;
+	        oneConnection.connected_id = val.connected_id;
+	        oneConnection.status = val.status;
+	        oneConnection.location_id = val.location_id;
+	        oneConnection.date_initiated = val.date_initiated;
+
+	        connections.push(oneConnection);
+	    }
+
+	    return connections;
     },
     async addConnection(connectionData){
-        const connectionCollection = connectionList();
+        const connectionCollection = await connectionList();
+
+	    const newConnection = {
+	        _id: uuidv1(),
+	        requestor_id: connectionData.requestor_id,
+	        connected_id: connectionData.connected_id,
+	        status: connectionData.status,
+	        location_id: connectionData.location_id,
+	        date_initiated: connectondata.date_initiated
+	    };
+
+	    console.log(newConnection);
+	    const newInsertInformation = await connectionCollection.insertOne(newConnection);
+	    const newId = newInsertedInformation.insertedId;
+	    console.log("inserted: "+newId);
+	    return await this.getConnetionById(newId);
 
     },
-    async deleteConnection(connectionID){
-        const connectionCollection = connectionList();
-
+    async removeConnection(_id){
+	    if(!_id) throw 'provide an id to delete';
+        const connectionCollection = await connectionList();
+	    const deletionInfo = await connectionCollection.removeOne({ _id: _id});
+	    if (deletionInfo.deletedCount ===0){
+	        throw `Could not delete user with id of ${_id}`;
+	    }
     }
 
 
--- ProjectEffugio/data/travel.js
+++ ProjectEffugio/data/travel.js
@@ -8,21 +8,54 @@ var bcrypt = Promise.promisifyAll(require("bcrypt"));
 
 
 let exportedMethods={
+    
 
-async getAllTravel(){
-    const travelConnection = travelList();
+    async getAllTravel(){
+    	const travelConnection = awaittravelList();
+	    const listOfTravels = await travelConnection.find().toArray();
+	    travels = [];
 
-},
-async getTravelByID(_id){
-    const travelConnection = travelList();
-},
-async getIDByLocation(_location){
-    const travelConnection = travelList();
-},
+	    for(var val of listOfTravels){
+	        oneTravel = {};
+	        oneTravel._id = val._id;
+	        oneTravel.name = val.name;
+	        oneTravel.description = val.description;
 
-async addTravelData(travelData){
-    const travelConnection = travelList();
-}
+	        travels.push(oneTravel);
+	    }
+
+	    return travels;
+    },
+    async getTravelById(_id){
+	
+	    if(!_id) throw 'provide id get the travel details';
+	    const travelConnection = await travelList();
+	    const listOfTravels = await travelConnection.find({_id: _id}).limit(1).toArray();
+	    if (listOfTravels.length ===0) return null;
+	    return listOfTravels[0];
+    },
+    async getIDByLocation(name){
+	    if(!name) throw 'provide a name to give the location details';
+	    const travelConnection = await travelList();
+	    const listOfTravels = await travelConnection.fnid({name: name}).limit(1).toArray();
+	    if (listOfTravels.length ===0) return null;
+	    return listOfTravels[0];
+    },
+
+    async addTravelData(travelData){
+	    const travelConnection = await travelList();
+	    const newTravel = {
+	        _id: uuidv1(),
+	        name: travelData.name,
+	        description: travelData.description
+	    };
+	    console.log(newTravel);
+	    const newInsertInformation = await travelConnection.insertOne(newTravel);
+	    const newId = newInsertedInformation.insertedId;
+	    console.log("inserted: "+newId);
+	    return await this.getTravelById(newId);
+	
+    }
 
 
 };
